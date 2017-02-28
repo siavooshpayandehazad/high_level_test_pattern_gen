6522f66b07bda71829c119f47f5eccca9b5a5a20
aysteph3 <aysteph3@yahoo.com>
2017-03-01 01:20:10 +0200

    Added time calculation, pattern file generation specifically for SAF(turbo-tester) for VLIW test

--- algorithm_2.py
+++ algorithm_2.py
@@ -1,8 +1,18 @@
 import Logger
-import sys
+import sys, os
 import copy
+import time
 
 sys.stdout = Logger.Logger()
+
+generated_files_folder = "generated_files"
+
+if os.path.exists("generated_files"):
+	file_list = [file for file in os.listdir("generated_files")]
+	for file in file_list:
+		os.remove("generated_files"+'/'+file)
+else:
+    os.mkdir("generated_files")
 
 if "--help" in sys.argv[1:] or len(sys.argv[1:]) == 0:
 	print "---------------------------------------------------------------------------"
@@ -11,7 +21,7 @@ if "--help" in sys.argv[1:] or len(sys.argv[1:]) == 0:
 	print "program arguments:"
 	print "-i [file name]: spcifies the path to the input file" 
 	#print "-ot [file name]: spcifies the path to the generated table file" 
-	#print "-op [file name]: spcifies the path to the generated patterns file" 
+	print "-op [file name]: spcifies the path to the generated patterns file" 
 	#print "-v: makes it more verbose" 
 	#print "-debug: enables debug printing"
 	print "---------------------------------------------------------------------------"
@@ -31,12 +41,12 @@ else:
 	debug = False
 
 if "-ot" in sys.argv[1:]:
-	output_table_file_name= generated_files_folder + "/" + sys.argv[sys.argv.index('-ot') + 1]
+	output_table_file_name= generated_files_folder + "/" + sys.argv[sys.argv.index('-ot') + 1]"""
 
 if "-op" in sys.argv[1:]:
-	output_patterns_file_name= generated_files_folder + "/" + sys.argv[sys.argv.index('-op') + 1]"""
-	
+	output_patterns_file_name= generated_files_folder + "/" + sys.argv[sys.argv.index('-op') + 1]
 
+start_time = time.time()
 function_dict = {}
 line_counter = 0
 with open(input_file_name) as f:
@@ -51,6 +61,7 @@ with open(input_file_name) as f:
 len_of_list = len(function_dict[function_dict.keys()[0]])
 number_of_lines = len(function_dict.keys())
 
+test_patterns_file = open(output_patterns_file_name, 'w')
 
 deletion_dic = {}
 used_dic = {}
@@ -100,6 +111,8 @@ for func_id_1 in range(2, len_of_list):
 				print  "INFO::  Didn't find a solution!"
 
 			print "final list of patterns:", list_of_necessary_patterns
+	
+
 			#print "final list of unused patterns:", list_of_pattens_to_delete
 			deletion_dic['{0:03}'.format(func_id_1)+"_"+'{0:03}'.format(func_id_2)] = copy.deepcopy(list_of_pattens_to_delete)
 			used_dic['{0:03}'.format(func_id_1)+"_"+'{0:03}'.format(func_id_2)] = copy.deepcopy(list_of_necessary_patterns)
@@ -111,6 +124,19 @@ for func_id_1 in range(2, len_of_list):
 								list_of_used_patterns.remove(item) 
 								#print "removed pattern no:", item
 
+	# Print patterns and functions.. This will be used to prepare test patterns for SAF testing in turbo tester
+	# This should only be used for VLIW experiment. Modification will be needed for other processors
+	print "-----------------------------------------------------"
+	print "function_1: ",func_id_1
+	opcode = "{0:04b}".format((func_id_1-2))
+	#test_patterns_file.write("function_1: "+str(func_id_1)+ " "+str(opcode)+"\n")
+	for j in list_of_necessary_patterns:
+		#test_patterns_file.write(str(j)+"\t"+function_dict[j][0]+"\t"+function_dict[j][1]+"\n")
+		test_patterns_file.write(function_dict[j][0]+function_dict[j][1]+opcode+"\n")
+	#test_patterns_file.write("\n")
+
+test_patterns_file.close()
+stop_time = time.time()
 print "final list of patterns:", list_of_used_patterns
 print "-----------------------------------------------------"
 print "list of possible removals for each pair of functions:"
@@ -123,3 +149,5 @@ for item in sorted(deletion_dic.keys()):
 	if counter == len_of_list-2:
 		print "------------------------------------------------------------"*2
 		counter = 1
+print "program took ", str(stop_time-start_time), "seconds"
+

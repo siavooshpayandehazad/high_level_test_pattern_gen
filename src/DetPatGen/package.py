012b4a61582f7a7ffff23b6ec10551e535e32b94
Siavoosh.Azad <siavoosh.azad@ttu.ee>
2017-03-22 18:17:16 +0200

    updated deterministic approach

--- src/DetPatGen/package.py
+++ src/DetPatGen/package.py
@@ -1,3 +1,6 @@
+from ALU import *
+
+
 op_dic = {
 	"mov" 	:	"0000" ,
 	"add" 	:	"0001" ,
@@ -19,3 +22,54 @@ op_dic = {
 
 # this list is used only to keep order of the op-codes... since dictionaries dont keep the keys in order!
 list_of_operations = ["mov","add","sub","cmp","and","or" ,"xor","not" ,"shl","shr","asr","inc","dec","rlc","rrc","nop"]
+
+
+def report_function_res(opt_patterns):
+	file = open('pattern_set.txt', 'w')
+	print "----------------------------------------------------"*3
+	print "printing function results:"
+	string = '%8s' %"OP1"+ " " + '%8s' %"OP2"
+	for func in list_of_operations:
+		string += " " +'%8s' %str(func) 
+	print string
+	print "---"*55
+
+	for pattern in opt_patterns:
+		string = "0000 " + numpy.binary_repr(pattern[0],8) + " " +numpy.binary_repr(pattern[1],8)+ " "
+		for func in list_of_operations:
+			string +=   numpy.binary_repr(alu(pattern[0], pattern[1], op_dic[func], 0), 8)[-8:]+ " "
+		#print string
+		file.write(string[:-1]+"\n")
+	file.close()
+
+def report_table(opt_patterns, print_to_console):
+	if print_to_console:
+		print "----------------------------------------------------"*3
+		print "reporting corss function table:"
+		print  "\t",
+		for func in list_of_operations:
+			print '%8s' %str(func),
+		print
+	number_of_ones = 0
+	for func1 in list_of_operations:
+		string = str(func1)+"\t"
+		for func2 in list_of_operations:
+			if func1 != func2:
+				or_val = "00000000"
+				for pattern in opt_patterns:
+					res_1 = alu(pattern[0], pattern[1], op_dic[func1], 0)	
+					res_2 = alu(pattern[0], pattern[1], op_dic[func2], 0)	
+					binary1 = numpy.binary_repr(res_1, 8)[-8: ]
+					binary2 = numpy.binary_repr(res_2, 8)[-8: ]
+					xor_value = numpy.binary_repr(numpy.bitwise_xor(int(binary1, 2), int(binary2, 2)), 8) [-8: ]
+					and_value = numpy.binary_repr(numpy.bitwise_and(int(xor_value, 2), int(binary2, 2)), 8) [-8: ]
+					or_val = numpy.binary_repr(numpy.bitwise_or(int(and_value, 2), int(or_val, 2)), 8) [-8: ]
+				string += or_val+" "
+				number_of_ones += or_val.count("1")
+			else:
+				string  += "XXXXXXXX"+" "
+		if print_to_console:
+			print string
+	if print_to_console:
+		print "number of ones in the table:", number_of_ones
+	return number_of_ones
--- src/DetPatGen/test_alu.py
+++ src/DetPatGen/test_alu.py
@@ -3,7 +3,6 @@ import numpy
 from package import *
 import sys
 import copy
-from back_track import back_track
 
 print "----------------------------------"
 print "description:"
@@ -20,8 +19,10 @@ carry = 0
 full_list_of_ops = copy.deepcopy(list_of_operations)
 k1_list = copy.deepcopy(list_of_operations)
 patterns_taken = []
+test_length = 0
 
 for key1 in k1_list:
+	current_patterns_taken = []
 	print "checking function:", key1
 	op_temp_dic = {
 					"mov" 	:	"00000000" , "add" 	:	"00000000" ,
@@ -60,18 +61,77 @@ for key1 in k1_list:
 					if int(op_temp_dic[key2],2) < int(temp_or, 2):
 						#print '%7s' %key1,'%7s' %key2, '%10s'%op_1_bin, '%10s'%op_2_bin,  "\t\t",'%10s'%binary1,'%10s'%binary2, '%15s'%xor_value, '%15s'%and_value, '%15s'%op_temp_dic[key2]
 						op_temp_dic[key2] = temp_or
+						
 						if (op1, op2) not in patterns_taken:
-							print "\tselecting pattern:", (op1, op2)
 							patterns_taken.append((op1, op2))
+						if (op1, op2) not in current_patterns_taken:
+							print "\tselecting pattern for current function:", (op1, op2)
+							current_patterns_taken.append((op1, op2))
+
 					if op_temp_dic[key2] == "11111111":
 						k2_list.remove(key2)
 						#print "function ", key2, "is covered!"		
 	del op_temp_dic
+	test_length += len(current_patterns_taken)
 	print "function:", key1, "fully examined!"
+	print "number of patterns for the current function:", len(current_patterns_taken)
 	print "number of patterns take so far:", len(patterns_taken)
 	print "----------------------------------------"*3
 
-#print len(patterns_taken)
-#print patterns_taken
-back_track(patterns_taken)
+ 
+print "test length:", test_length
+report_table(patterns_taken, True)
 
+print "-------------------------------------------------"
+final_pattern_list = []
+old_number_of_ones = 0
+for pattern in patterns_taken:
+	number_of_ones = report_table(final_pattern_list+[pattern], False)
+	if number_of_ones > old_number_of_ones:
+		print "selecting pattern", pattern, "number of ones improved to:", number_of_ones 
+		final_pattern_list.append(pattern)
+		old_number_of_ones = number_of_ones
+	else:
+		print "not picking pattern:", pattern
+print "-------------------------------------------------"
+print "number of patterns:", len(final_pattern_list)
+
+report_function_res(final_pattern_list)
+print "final list:", final_pattern_list
+report_table(final_pattern_list, True)
+
+print "-------------------------------------------------"
+
+total_test_length = 0
+for key1 in list_of_operations:
+	func_pattern = []
+	#print "finding test length for function", key1
+	for key2 in list_of_operations:
+		if key1 != key2:
+			temp_or = "00000000"
+			test_length  = 0
+			for pattern in final_pattern_list:
+				
+				func1 = alu(pattern[0], pattern[1], op_dic[key1], 0)	
+				func2 = alu(pattern[0], pattern[1], op_dic[key2], 0)	
+				binary1 = numpy.binary_repr(func1, 8)[-8: ]
+				binary2 = numpy.binary_repr(func2, 8)[-8: ]
+				
+				xor_value = numpy.binary_repr(numpy.bitwise_xor(int(binary1, 2), int(binary2, 2)), 8) [-8: ]
+				and_value = numpy.binary_repr(numpy.bitwise_and(int(xor_value, 2), int(binary2, 2)), 8) [-8: ]
+				new_or =  numpy.bitwise_or(int(temp_or,2), int(and_value,2))
+				if int(temp_or,2) < new_or:
+					#print key1, key2, numpy.binary_repr(pattern[0],8), numpy.binary_repr(pattern[1],8), binary1, binary2, xor_value, and_value, numpy.binary_repr(new_or)
+					temp_or = numpy.binary_repr(new_or) [-8: ]
+					if pattern not in func_pattern:
+						
+						func_pattern.append(pattern)
+					#print "\tpattern taken:", numpy.binary_repr(pattern[0],8), numpy.binary_repr(pattern[1],8)
+				if temp_or == "11111111": 
+					break
+			#print "-------------"
+	print "test length for function", key1, "is:", len(func_pattern) 
+	#print func_pattern
+	total_test_length +=  len(func_pattern) 
+print "-------------------------------------------"
+print "total test length:",total_test_length

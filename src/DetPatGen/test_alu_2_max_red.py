76b63e0bc418cc4ed78bc06475ebd24ce1ce6af8
Siavoosh Payandeh Azad <siavooshpayandeh@gmail.com>
2017-03-27 09:53:01 +0300

    patterns added

--- src/DetPatGen/test_alu_2_max_red.py
+++ src/DetPatGen/test_alu_2_max_red.py
@@ -13,7 +13,7 @@ selected_patterns = [(172, 55), (83, 201),  (150, 242), (105, 14),
 					 (53, 77),  (200, 177), (127, 126), (0, 7), 
 					 (91, 139), (111, 162), (95, 130),  (191, 2),
 					 (128, 255), (255, 130), (151, 98), (176, 31),
-					 (160, 63), (192, 127)]
+					 (160, 63), (192, 127), (88, 143)]
 last_round_most_number_of_ones = report_table(selected_patterns, False)
 max_number_of_ones = last_round_most_number_of_ones
 
--- src/DetPatGen/test_alu_3.py
+++ src/DetPatGen/test_alu_3.py
@@ -5,11 +5,20 @@ import sys
 import copy
 
 max_number_of_ones = 0
-selected_patterns = []
-
+selected_patterns = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 8), (0, 9), 
+			 		 (0, 16), (0, 17), (0, 32), (0, 33), (0, 64), (0, 65), (0, 128), 
+			 		 (0, 129), (1, 0), (1, 1), (1, 2), (1, 3), (1, 4), (1, 7), (1, 8), 
+			 		 (1, 15), (1, 16), (1, 31), (1, 32), (1, 63), (1, 64), (1, 127), 
+			 		 (1, 128), (1, 255), (2, 0), (2, 1), (2, 2), (2, 4), (3, 0), (3, 2), 
+			 		 (3, 4), (4, 0), (4, 1), (4, 2), (4, 4), (4, 8), (5, 0), (7, 0), 
+			 		 (7, 4), (7, 8), (8, 0), (8, 1), (8, 4), (8, 8), (8, 16), (9, 0), 
+			 		 (15, 0), (15, 8), (15, 16), (16, 0), (16, 1), (16, 8), (16, 16), 
+			 		 (16, 32), (17, 0), (31, 0), (31, 16), (31, 32), (32, 0), (32, 1), 
+			 		 (32, 16), (32, 32), (32, 64), (33, 0)]
+max_number_of_ones = report_table(selected_patterns, False)
 
 counter = 0
-for op_1 in range(0, 256):
+for op_1 in range(33, 256):
 	for op_2 in range(0, 256):
 		counter += 1
 		pattern = [(op_1, op_2)]
@@ -17,6 +26,7 @@ for op_1 in range(0, 256):
 			number_of_ones = report_table(selected_patterns+pattern, False)
 			if number_of_ones > max_number_of_ones:
 				print "found better solution with ", len(selected_patterns+pattern),"patterns with", number_of_ones, "ones! patterns counted:", counter
+				print selected_patterns+pattern
 				#report_table(selected_patterns+pattern, False)
 				max_number_of_ones = number_of_ones
 				best_pattern = pattern
